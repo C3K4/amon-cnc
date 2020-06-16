5ce2cc5b5469f873e93953c021cc1c6adbed4b55
marko cekic <cekanet@hotmail.com>
2020-06-16 12:28:26 +0200

    Added withRouter HOC

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
 
 import './category-item.styles.scss';
 
-const CategoryItem = ({title, imageUrl,size}) => (
-    <div  className= {`${size} category-item`}>
+const CategoryItem = ({title, imageUrl,size,history,linkUrl,match}) => (
+    <div  className= {`${size} category-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
     <div className='content'>
         <h1 className='title'>{title.toUpperCase()}</h1>
@@ -12,4 +13,4 @@ const CategoryItem = ({title, imageUrl,size}) => (
 </div>
 );
 
-export default CategoryItem;
+export default withRouter(CategoryItem);
